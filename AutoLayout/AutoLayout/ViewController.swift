bbcd48e406874b71cced57ba84dc0f1681e7375b
jmbae <jmbae@codegrove.co.kr>
2024-05-20 09:34:30 +0900

    AutoLayout

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Jungman Bae on 5/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let label = UILabel()
+        label.text = "Hello, Auto Layout"
+        label.translatesAutoresizingMaskIntoConstraints = true
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+    }
+
+
+}
+
