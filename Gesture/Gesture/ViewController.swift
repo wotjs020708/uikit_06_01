1f3247bba0515bfc3b9749e06f3aa8a35dcb5f25
JaeSunEo <wotjs020708@gmail.com>
2024-05-22 10:34:28 +0900

    clear

--- Gesture/Gesture/ViewController.swift
+++ Gesture/Gesture/ViewController.swift
@@ -10,31 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-            
-        let imageView = UIImageView(image: UIImage(systemName: "star.fill"))
-        imageView.frame = CGRect(x: 120, y: 300, width: 200, height: 200)
-        imageView.isUserInteractionEnabled = true
-        
-        view.addSubview(imageView)
-        
-        let gesture = UIPinchGestureRecognizer(target: self, action: #selector(handleGesture))
-        imageView.addGestureRecognizer(gesture)
-    }
-    
-    @objc func handleGesture(_ sender: UIPinchGestureRecognizer){
-        print("\(sender.scale)")
-        guard let view = sender.view else { return }
-        let currentScale = sqrt(view.transform.a * view.transform.a + view.transform.c * view.transform.c)
-        if sender.scale < 1.0 {
-            if currentScale > 0.8 {
-                view.transform = view.transform.scaledBy(x: sender.scale, y: sender.scale)
-            }
-        } else {
-            if currentScale < 1.2 {
-                view.transform = view.transform.scaledBy(x: sender.scale, y: sender.scale)
-            }
-        }
+
     }
     
 
