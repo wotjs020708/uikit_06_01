ff4e0a9353c13363922e09721e2fb975b576f8de
JaeSunEo <wotjs020708@gmail.com>
2024-05-22 09:31:20 +0900

    TapGesture

--- Gesture/Gesture/ViewController.swift
+++ Gesture/Gesture/ViewController.swift
@@ -11,9 +11,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            
+            
+            let rectangle = UIView()
+            rectangle.backgroundColor = .yellow
+            rectangle.frame = CGRect(x: 100, y: 100, width: 175, height: 125)
+            rectangle.isUserInteractionEnabled = true
+            
+            view.addSubview(rectangle)
+            
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+            tapGesture.numberOfTapsRequired = 2
+            rectangle.addGestureRecognizer(tapGesture)
     }
 
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        if let view = sender.view {
+            view.backgroundColor = (view.backgroundColor == .yellow) ? .red : .yellow
+        }
+    }
+    
 
 }
 
