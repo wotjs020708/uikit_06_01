127237f10fb7f7176f2c247e3992746654a75fcb
JaeSunEo <wotjs020708@gmail.com>
2024-05-22 14:37:10 +0900

    단순한 목록 with TableView ,CellRegister

--- List/List/ViewController.swift
+++ List/List/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    let items = ["고양이", "강아지", "새", " 파출류", "물고기"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let tableview = UITableView(frame:  view.bounds)
+        tableview.dataSource = self
+        tableview.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(tableview)
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .default, reuseIdentifier: "cell")
+        var config = cell.defaultContentConfiguration()
+        config.text = items[indexPath.row]
+        cell.contentConfiguration = config
+        
+        return cell
+    }
+    
 
 }
 
