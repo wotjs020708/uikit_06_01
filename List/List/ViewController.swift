ca96bdad24a4d43efd649548ad753f8e490f076b
JaeSunEo <wotjs020708@gmail.com>
2024-05-23 09:32:35 +0900

    image가 있는 테이블뷰

--- List/List/ViewController.swift
+++ List/List/ViewController.swift
@@ -7,11 +7,44 @@
 
 import UIKit
 
+struct Animal {
+    let name: String
+    let image: UIImage
+}
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UITableViewDataSource {
+    
+
+    let animals = [
+    Animal(name: "고양이", image: UIImage(systemName: "cat")!),
+    Animal(name: "강아지", image: UIImage(systemName: "dog")!),
+    Animal(name: "토끼", image: UIImage(systemName: "hare")!)
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tableview = UITableView(frame: view.bounds)
+        tableview.dataSource = self
+        tableview.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(tableview)
+    }
+    
+    // MARK: - UITableVeiwDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var config = cell.defaultContentConfiguration()
+        config.text = animals[indexPath.row].name
+        config.image = animals[indexPath.row].image
+        cell.contentConfiguration = config
+        
+        
+        return cell
     }
 }
 
